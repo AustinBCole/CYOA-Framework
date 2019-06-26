e506615ff6e9d7502665a1c6fca6460997f15a82
AustinBCole <austin.cole.chileno@gmail.com>
2019-06-26 11:01:49 -0600

    Add createLabel method.

--- MexicanTreasure/MexicanTreasure/SceneViewController.swift
+++ MexicanTreasure/MexicanTreasure/SceneViewController.swift
@@ -16,11 +16,32 @@ class SceneViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    //MARK: Public Variables
     
     
+    //MARK: Private Variables
     
+    
+    //MARK: Public Methods
+    
+    public func createScene(text: String, font: UIFont = UIFont(name: "Helvetica", size: 20)!) {
+        let height = getHeightForLabel(text: text, font: font)
+        createLabel(height: height, text: text, font: font)
+    }
     
     //MARK: Private Methods
+    
+    private func createLabel(height: CGFloat, text: String, font: UIFont) {
+        let label = UILabel(frame: CGRect(x: 8, y: 8, width: view.frame.width - 16, height: height))
+        label.numberOfLines = 0
+        label.lineBreakMode = NSLineBreakMode.byWordWrapping
+        label.font = font
+        label.text = text
+        
+        label.sizeToFit()
+        
+        view.addSubview(label)
+    }
     
     private func getHeightForLabel(text: String, font: UIFont) -> CGFloat {
         let label = UILabel(frame: CGRect(x: 8, y: 8, width: view.frame.width - 16, height: CGFloat.greatestFiniteMagnitude))
